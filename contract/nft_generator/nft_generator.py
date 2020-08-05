8edcc509bdd3d933cb030483e1cba81545299bb5
anuprshetty <anuprshetty@gmail.com>
2020-08-05 20:47:46 +0530

    generated nft images

---
+++ contract/nft_generator/nft_generator.py
@@ -0,0 +1,13 @@
+import os
+from shutil import copyfile
+
+file_path = os.path.join(os.path.dirname(__file__), "tom_and_jerry.png")
+folder_path = os.path.join(os.path.dirname(__file__), "images")
+num_copies = 50
+
+if not os.path.exists(folder_path):
+    os.makedirs(folder_path)
+
+for i in range(num_copies):
+    new_file_path = os.path.join(folder_path, str(i+1) + ".png")
+    copyfile(file_path, new_file_path)
