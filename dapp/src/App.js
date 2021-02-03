4edcf621b5b5b5cc81f491a0dbb61f0d01849706
anuprshetty <anuprshetty@gmail.com>
2021-02-03 22:43:31 +0530

    implemented connectWallet functionality in dapp for metamask

--- dapp/src/App.js
+++ dapp/src/App.js
@@ -147,6 +147,72 @@ export default class App extends Component {
     }));
   };
 
+  /*
+  - Class methods definition as arrow functions:
+    -- Due to ES6 (ECMAScript 6) version of JavaScript, use arrow functions to define methods inside the class to automatically bind the method to the class instance. 
+    -- So that the 'this' keyword referenced inside the method will always refer to the class instance in which the method is defined, no matter how the function is called or passed as a callback.
+  - Class methods definition as regular functions (without arrow functions):
+    -- If the method is defined regularly without arrow function (like, async connectWallet(){}), then the value of 'this' keyword inside the method depends on how the function is called. 
+    -- If the function is called as a method of an object, then 'this' keyword refers to the object itself. However, if the function is called standalone or passed as a callback, then 'this' keyword can change its value or become undefined, leading to potential bugs. 
+    -- So if you define a method regularly without arrow function and you want that 'this' keyword referenced inside the method to always refer to the class instance in which the method is defined, then you need to explicitly bind the method to the class instance inside the constructor (like this, this.connectWallet = this.connectWallet.bind(this);).
+  */
+  connectWallet = async () => {
+    /*
+    - Ethereum Provider API --> It is a Javascript API which internally uses JSON-RPC protocol to interact with the Ethereum compatible blockchain network or node.
+  
+    - window.ethereum --> It is an Ethereum Provider API object injected by Metamask.
+      - Here window.ethereum(Ethereum Provider API) internally uses a third party network provider API(Ex: Infura, as configured in Metamask settings). That third party API will internally use the JSON-RPC protocol to interact with the Ethereum netowrk.
+      - By default, Metamask uses Infura as remote procedure call (RPC) provider(aka network provider) to offer the most reliable and private access to Ethereum data.
+      - If you are using the Metamask browser extension, it automatically injects the window.ethereum object into the browser's JavaScript environment.
+      - When a user has Metamask installed and active, the window.ethereum object is available for your web application to use.
+      - If window.ethereum == undefined or null, it means that Metamask extension is not installed or not active.
+    */
+    if (window.ethereum) {
+      /*
+      Providers in web3.js:
+        1. HttpProvider:
+          - This provider allows connecting to a remote Ethereum node via HTTP or HTTPS.
+          - It requires specifying the URL of the Ethereum node as the provider endpoint.
+          - The HttpProvider is useful when interacting with public Ethereum networks or when connecting to a locally running Ethereum node.
+          - Ex: const web3 = new Web3(new Web3.providers.HttpProvider('https://mainnet.infura.io/v3/API_KEY'));
+        2. WebsocketProvider:
+          - This provider establishes a WebSocket connection with an Ethereum node that support WebSocket connections.
+          - WebSocket is a communication protocol that provides full-duplex communication channels over a single TCP connection. WebSocket allows for efficient and low-latency data exchange, making it well-suited for real-time applications.
+          - It enables real-time bidirectional communication with the Ethereum node, allowing for subscriptions to events, real-time updates, and efficient communication with the Ethereum network.
+          - The WebsocketProvider is commonly used when working with real-time data or building applications that require constant updates from the blockchain.
+          - Ex: const web3 = new Web3(new Web3.providers.WebsocketProvider('wss://mainnet.infura.io/v3/API_KEY'));
+        3. IpcProvider:
+          - This provider allows connecting to a locally running Ethereum node using an Inter-Process Communication (IPC) file.
+          - The IPCProvider is commonly used when interacting with a local Ethereum node running on the same machine as the application.
+          - Ex: const web3 = new Web3(new Web3.providers.IpcProvider('/path/to/geth.ipc', require('net')));
+        4. MetamaskProvider:
+          - It is a specific type of provider designed to interact with the Ethereum network through the Metamask browser extension.
+          - Internally, the MetamaskProvider can use different underlying communication protocols, such as HTTP or WebSockets, depending on how the Metamask extension is configured. It abstracts away the specific details of the communication protocol and exposes a unified interface for interacting with the Ethereum network.
+          - When a user has Metamask installed and enabled in their browser, the MetamaskProvider can be used to interact with the Ethereum network through the user's Metamask accounts.
+          - Web3.js sets up the necessary communication channels with Metamask to send requests, receive responses, and handle Ethereum-related operations.
+          - Ex: const web3 = new Web3(window.ethereum);
+        5. Custom Providers:
+          - Web3.js also allows creating custom providers to connect to specific Ethereum-compatible networks or other custom setups.
+          - Custom providers can implement the necessary interfaces with communication logic to connect to the network and interact with it.
+          - This requires advanced knowledge and skillset.
+          - Ex: const web3 = new Web3(new MyCustomProvider('https://customnetwork.example.com'));
+      */
+      let web3 = new Web3(window.ethereum);
+
+      await this.refreshChain();
+      await this.refreshAccounts();
+
+      window.ethereum.on("chainChanged", this.refreshPage);
+      window.ethereum.on("accountsChanged", this.refreshAccounts);
+
+      var nftMinterABI = NFTMinter.abi;
+      var nftMinterAddress = process.env.REACT_APP_NFT_MINTER_ADDRESS;
+      this.contract = new web3.eth.Contract(nftMinterABI, nftMinterAddress);
+    } else {
+      console.log("Please install Metamask");
+    }
+  };
+
 
 
   render() {
