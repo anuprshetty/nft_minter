964a318cb9712e7a5856f1121b924d844d618da0
anuprshetty <anuprshetty@gmail.com>
2023-05-12 13:54:09 +0530

    NFT mint portal frontend

--- src/App.js
+++ src/App.js
@@ -6,19 +6,43 @@ import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>This is NFT Viewer Application</p>
-        <Button>Mint</Button>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="row">
+          <form
+            class="gradient col-lg-5 mt-5"
+            style={{ borderRadius: "25px", boxShadow: "1px 1px 15px #000000" }}
+          >
+            <h4 style={{ color: "#FFFFFF" }}>Mint Portal</h4>
+            <h5 style={{ color: "#FFFFFF" }}>Please connect your wallet</h5>
+            <Button style={{ marginBottom: "5px", color: "#FFFFFF" }}>
+              Connect Wallet
+            </Button>
+            <div
+              class="card"
+              id="wallet-address"
+              style={{ marginTop: "3px", boxShadow: "1px 1px 4px #000000" }}
+            >
+              <label style={{ color: "#FFFFFF" }} for="floating Input">
+                Wallet Address
+              </label>
+              <input
+                type="number"
+                name="amount"
+                defaultValue="1"
+                min="1"
+                max="5"
+              />
+              <label style={{ color: "#FFFFFF" }}>
+                Please select the amount of NFTS to mint.
+              </label>
+              <Button>Mint/Buy</Button>
+            </div>
+            <label style={{ color: "#FFFFFF" }}>
+              Price 0.06 ETH each mint.
+            </label>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
