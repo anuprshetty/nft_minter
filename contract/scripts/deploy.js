5e55d52b05440487bcdd33c277e99e2b6b54febb
anuprshetty <anuprshetty@gmail.com>
2024-01-30 11:46:53 +0530

    contracts_setup_inputs.json file validity check

--- contract/scripts/deploy.js
+++ contract/scripts/deploy.js
@@ -270,7 +270,27 @@ class SetupE2E extends BaseDeploy {
     await this.setBaseURI();
   }
 
+  async setupPrerequisites() {
+    const all_contracts_setup_inputs = JSON.parse(
+      fs.readFileSync(
+        path.join(__dirname, "..", "contracts_setup_inputs.json"),
+        "utf8"
+      )
+    );
 
+    for (let contracts_setup_inputs in all_contracts_setup_inputs) {
+      contracts_setup_inputs =
+        all_contracts_setup_inputs[contracts_setup_inputs];
+      for (let contract_setup_inputs in contracts_setup_inputs) {
+        contract_setup_inputs = contracts_setup_inputs[contract_setup_inputs];
+        if (!contract_setup_inputs.address) {
+          throw new Error(`contracts_setup_inputs.json file is invalid.`);
+        }
+      }
+    }
+
+    
+  }
 }
 
 
